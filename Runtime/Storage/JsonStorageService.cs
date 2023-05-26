bb52c5f804ca6be33cfba5491c9508c52e946e33
Christian Hanne <support@aureola.codes>
2023-05-26 20:52:05 +0200

    Add getter methods to storage service

--- Runtime/Storage/JsonStorageService.cs
+++ Runtime/Storage/JsonStorageService.cs
@@ -83,13 +83,85 @@ namespace Aureola.Storage
             Save();
         }
 
-        public int GetInt(string key, int defaultValue = 0)
+        public int GetInt(string key, int defaultValue)
         {
             if (!_cache.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return (int)_cache[key];
+            return (int) _cache[key];
+        }
+
+        public float GetFloat(string key, float defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (float) _cache[key];
+        }
+
+        public string GetString(string key, string defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (string) _cache[key];
+        }
+
+        public bool GetBool(string key, bool defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (bool) _cache[key];
+        }
+
+        public Vector2 GetVector2(string key, Vector2 defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (Vector2) _cache[key];
+        }
+
+        public Vector3 GetVector3(string key, Vector3 defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (Vector3) _cache[key];
+        }
+
+        public Vector4 GetVector4(string key, Vector4 defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (Vector4) _cache[key];
+        }
+
+        public Color GetColor(string key, Color defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (Color) _cache[key];
+        }
+
+        public Color32 GetColor32(string key, Color32 defaultValue)
+        {
+            if (!_cache.ContainsKey(key)) {
+                return defaultValue;
+            }
+
+            return (Color32) _cache[key];
         }
 
         private void Load()
