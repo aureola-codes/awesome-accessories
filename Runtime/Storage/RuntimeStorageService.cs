5c3279fbbd10d47c888a5a2f6eccff7eeb7f40e0
Christian Hanne <support@aureola.codes>
2023-05-27 11:08:35 +0200

    Refactor RuntimeStorageService

--- Runtime/Storage/RuntimeStorageService.cs
+++ Runtime/Storage/RuntimeStorageService.cs
@@ -5,169 +5,151 @@ namespace Aureola.Storage
 {
     public class RuntimeStorageService : IStorageService
     {
-        private Dictionary<string, bool> _boolValues = new Dictionary<string, bool>();
-        private Dictionary<string, float> _floatValues = new Dictionary<string, float>();
-        private Dictionary<string, int> _intValues = new Dictionary<string, int>();
-        private Dictionary<string, string> _stringValues = new Dictionary<string, string>();
-        private Dictionary<string, Vector2> _vector2Values = new Dictionary<string, Vector2>();
-        private Dictionary<string, Vector3> _vector3Values = new Dictionary<string, Vector3>();
-        private Dictionary<string, Vector4> _vector4Values = new Dictionary<string, Vector4>();
-        private Dictionary<string, Color> _colorValues = new Dictionary<string, Color>();
-        private Dictionary<string, Color32> _color32Values = new Dictionary<string, Color32>();
-        private Dictionary<string, Quaternion> _quaternionValues = new Dictionary<string, Quaternion>();
+        private Dictionary<string, object> _values = new Dictionary<string, object>();
 
         public void Set(string key, int value)
         {
-            _intValues[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, float value)
         {
-            _floatValues[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, string value)
         {
-            _stringValues[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, bool value)
         {
-            _boolValues[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, Vector2 value)
         {
-            _vector2Values[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, Vector3 value)
         {
-            _vector3Values[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, Vector4 value)
         {
-            _vector4Values[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, Color value)
         {
-            _colorValues[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, Color32 value)
         {
-            _color32Values[key] = value;
+            _values[key] = value;
         }
 
         public void Set(string key, Quaternion value)
         {
-            _quaternionValues[key] = value;
+            _values[key] = value;
         }
 
         public int Get(string key, int defaultValue)
         {
-            if (!_intValues.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _intValues[key];
+            return (int) _values[key];
         }
 
         public float Get(string key, float defaultValue)
         {
-            if (!_floatValues.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _floatValues[key];
+            return (float) _values[key];
         }
 
         public string Get(string key, string defaultValue)
         {
-            if (!_stringValues.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _stringValues[key];
+            return (string) _values[key];
         }
 
         public bool Get(string key, bool defaultValue)
         {
-            if (!_boolValues.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _boolValues[key];
+            return (bool) _values[key];
         }
 
         public Vector2 Get(string key, Vector2 defaultValue)
         {
-            if (!_vector2Values.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _vector2Values[key];
+            return (Vector2) _values[key];
         }
 
         public Vector3 Get(string key, Vector3 defaultValue)
         {
-            if (!_vector3Values.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _vector3Values[key];
+            return (Vector3) _values[key];
         }
 
         public Vector4 Get(string key, Vector4 defaultValue)
         {
-            if (!_vector4Values.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _vector4Values[key];
+            return (Vector4) _values[key];
         }
 
         public Color Get(string key, Color defaultValue)
         {
-            if (!_colorValues.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _colorValues[key];
+            return (Color) _values[key];
         }
 
         public Color32 Get(string key, Color32 defaultValue)
         {
-            if (!_color32Values.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _color32Values[key];
+            return (Color32) _values[key];
         }
 
         public Quaternion Get(string key, Quaternion defaultValue)
         {
-            if (!_quaternionValues.ContainsKey(key)) {
+            if (!_values.ContainsKey(key)) {
                 return defaultValue;
             }
 
-            return _quaternionValues[key];
+            return (Quaternion) _values[key];
         }
 
         public void Clear()
         {
-            _boolValues.Clear();
-            _floatValues.Clear();
-            _intValues.Clear();
-            _stringValues.Clear();
-            _vector2Values.Clear();
-            _vector3Values.Clear();
-            _vector4Values.Clear();
-            _colorValues.Clear();
-            _color32Values.Clear();
-            _quaternionValues.Clear();
+            _values.Clear();
         }
     }
 }
