b7b7c3142d967f52bb9ff4bec48dff2ec6fc4fc6
Christian Hanne <support@aureola.codes>
2023-05-28 15:32:17 +0200

    Add documentation for LoaderService

--- Runtime/Loader/LoaderService.cs
+++ Runtime/Loader/LoaderService.cs
@@ -7,39 +7,31 @@ namespace Aureola.Loader
     public class LoaderService
     {
         private Dictionary<string, float> _progress = new Dictionary<string, float>();
+        private float _totalProgress = 0;
 
         public delegate void OnProgress(float progress);
         public delegate void OnComplete();
         public OnProgress onProgress;
         public OnComplete onComplete;
 
-        public float progress
-        {
-            get {
-                float total = 0;
-                foreach (var item in _progress) {
-                    total += item.Value;
-                }
-                return total / _progress.Count;
-            }
-        }
+        public float progress => _totalProgress;
 
         public void Add(string name)
         {
             _progress.Add(name, 0);
-            Broadcast();
+            Update();
         }
 
         public void Remove(string name)
         {
             _progress.Remove(name);
-            Broadcast();
+            Update();
         }
 
         public void SetProgress(string name, float progress)
         {
             _progress[name] = Mathf.Clamp01(progress);
-            Broadcast();
+            Update();
         }
 
         public void Complete(string name)
@@ -55,13 +47,32 @@ namespace Aureola.Loader
         public void Reset()
         {
             _progress.Clear();
-            Broadcast();
+            Update();
         }
 
-        private void Broadcast()
+        private void Update()
         {
-            onProgress?.Invoke(progress);
-            if (progress == 1) {
+            CalculateProgress();
+            BroadcastProgress();
+        }
+
+        private void CalculateProgress()
+        {
+            _totalProgress = 0;
+            if (_progress.Count > 0) {
+                float total = 0;
+                foreach (var item in _progress) {
+                    total += item.Value;
+                }
+
+                _totalProgress = total / _progress.Count;
+            }
+        }
+
+        private void BroadcastProgress()
+        {
+            onProgress?.Invoke(_totalProgress);
+            if (_totalProgress == 1) {
                 onComplete?.Invoke();
             }
         }
