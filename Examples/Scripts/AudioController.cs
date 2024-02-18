38a037e2c0d3d93636fb4ea57c48f98d47509837
Christian Hanne <support@aureola.codes>
2024-02-18 14:58:22 +0100

    Add example scene for audio behaviours

---
+++ Examples/Scripts/AudioController.cs
@@ -0,0 +1,20 @@
+using Aureola.Audio;
+using UnityEngine;
+
+namespace Aureola.Examples
+{
+    public class AudioController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
