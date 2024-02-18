dc62296306793e2d1cfd64b177206b74e464badb
Christian Hanne <support@aureola.codes>
2024-02-18 16:03:33 +0100

    Create example scene for config manager

--- Modules/Config/Scripts/Shared/ConfigManager.cs
+++ Modules/Config/Scripts/Shared/ConfigManager.cs
@@ -1,6 +1,5 @@
 using Aureola.Cache;
 using SimpleJSON;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -9,14 +8,24 @@ namespace Aureola.Config
     [CreateAssetMenu(fileName = "ConfigManager", menuName = "Aureola/Config/ConfigManager", order = 2)]
     public class ConfigManager : ScriptableObject, ILocatable
     {
-        public delegate void OnLoaded();
-        public OnLoaded onLoaded;
+        private RuntimeCache _storage;
+        private RuntimeCache Storage
+        {
+            get
+            {
+                if (_storage == null) {
+                    _storage = CreateInstance<RuntimeCache>();
+                }
+
+                return _storage;
+            }
+        }
+
+        public delegate void Loaded();
+        public Loaded OnLoaded;
 
         [Header("Settings")]
         [SerializeField] private AssetReference _configFile;
-
-        [Header("Dependencies")]
-        [SerializeField] private RuntimeCache _storage;
 
         public void Load()
         {
@@ -25,41 +34,62 @@ namespace Aureola.Config
                 foreach (var keyValuePair in jsonObject) {
                     var type = keyValuePair.Value.GetType().ToString().ToLower();
                     switch (type) {
-                        case "int":
-                            Set(keyValuePair.Key, (int) keyValuePair.Value);
+                        case "simplejson.jsonnumber":
+                            if (keyValuePair.Value.ToString().Contains(".")) {
+                                Storage.Set(keyValuePair.Key, (float) keyValuePair.Value);
+                            } else {
+                                Storage.Set(keyValuePair.Key, (int) keyValuePair.Value);
+                            }
                             break;
-                        case "float":
-                            Set(keyValuePair.Key, (float) keyValuePair.Value);
+                        case "simplejson.jsonstring":
+                            if (IsHexColorString((string) keyValuePair.Value)) {
+                                Storage.Set(keyValuePair.Key, HexToColor((string) keyValuePair.Value));
+                            } else if (IsRgbaColorString((string) keyValuePair.Value)) {
+                                Storage.Set(keyValuePair.Key, RgbaToColor((string) keyValuePair.Value));
+                            } else {
+                                Storage.Set(keyValuePair.Key, (string) keyValuePair.Value);
+                            }
                             break;
-                        case "string":
-                            Set(keyValuePair.Key, (string) keyValuePair.Value);
+                        case "simplejson.jsonbool":
+                            Storage.Set(keyValuePair.Key, (bool) keyValuePair.Value);
                             break;
-                        case "bool":
-                            Set(keyValuePair.Key, (bool) keyValuePair.Value);
+                        case "simplejson.jsonarray":
+                            if (keyValuePair.Value.Count == 2) {
+                                Storage.Set(keyValuePair.Key, new Vector2(keyValuePair.Value[0], keyValuePair.Value[1]));
+                            } else if (keyValuePair.Value.Count == 3) {
+                                Storage.Set(keyValuePair.Key, new Vector3(keyValuePair.Value[0], keyValuePair.Value[1], keyValuePair.Value[2]));
+                            } else if (keyValuePair.Value.Count == 4) {
+                                Storage.Set(keyValuePair.Key, new Quaternion(keyValuePair.Value[0], keyValuePair.Value[1], keyValuePair.Value[2], keyValuePair.Value[3]));
+                            } else {
+                                Debug.LogWarning("Unknown array type: " + keyValuePair.Value.ToString());
+                            }
                             break;
-                        case "vector2":
-                            Set(keyValuePair.Key, (Vector2) keyValuePair.Value);
+                        case "simplejson.jsonobject":
+                            if (IsColor(keyValuePair.Value)) {
+                                Storage.Set(keyValuePair.Key, new Color(
+                                    (byte) keyValuePair.Value["r"], 
+                                    (byte) keyValuePair.Value["g"], 
+                                    (byte) keyValuePair.Value["b"], 
+                                    (byte) keyValuePair.Value["a"]
+                                ));
+                            } else if (IsVector2(keyValuePair.Value)) {
+                                Storage.Set(keyValuePair.Key, new Vector2(keyValuePair.Value["x"], keyValuePair.Value["y"]));
+                            } else if (IsVector3(keyValuePair.Value)) {
+                                Storage.Set(keyValuePair.Key, new Vector3(keyValuePair.Value["x"], keyValuePair.Value["y"], keyValuePair.Value["z"]));
+                            } else if (IsQuaternion(keyValuePair.Value)) {
+                                Storage.Set(keyValuePair.Key, new Quaternion(keyValuePair.Value["x"], keyValuePair.Value["y"], keyValuePair.Value["z"], keyValuePair.Value["w"]));
+                            } else {
+                                Debug.LogWarning("Unknown object type: " + keyValuePair.Value.ToString());
+                            }
                             break;
-                        case "vector3":
-                            Set(keyValuePair.Key, (Vector3) keyValuePair.Value);
-                            break;
-                        case "vector4":
-                            Set(keyValuePair.Key, (Vector4) keyValuePair.Value);
-                            break;
-                        case "quaternion":
-                            Set(keyValuePair.Key, (Quaternion) keyValuePair.Value);
-                            break;
-                        case "color":
-                            Set(keyValuePair.Key, (Color) keyValuePair.Value);
-                            break;
-                        case "color32":
-                            Set(keyValuePair.Key, (Color32) keyValuePair.Value);
+                        default:
+                            Debug.LogWarning("Unknown type: " + type);
                             break;
                     }
                 }
 
                 Addressables.Release(handle);
-                onLoaded?.Invoke();
+                OnLoaded?.Invoke();
             };
         }
 
@@ -72,28 +102,71 @@ namespace Aureola.Config
             Load();
         }
 
-        public void Set(string key, float value) => _storage.Set(key, value);
-        public void Set(string key, string value) => _storage.Set(key, value);
-        public void Set(string key, int value) => _storage.Set(key, value);
-        public void Set(string key, bool value) => _storage.Set(key, value);
-        public void Set(string key, Vector2 value) => _storage.Set(key, value);
-        public void Set(string key, Vector3 value) => _storage.Set(key, value);
-        public void Set(string key, Vector4 value) => _storage.Set(key, value);
-        public void Set(string key, Quaternion value) => _storage.Set(key, value);
-        public void Set(string key, Color value) => _storage.Set(key, value);
-        public void Set(string key, Color32 value) => _storage.Set(key, value);
+        public float Get(string key, float defaultValue) => Storage.Get(key, defaultValue);
+        public string Get(string key, string defaultValue) => Storage.Get(key, defaultValue);
+        public int Get(string key, int defaultValue) => Storage.Get(key, defaultValue);
+        public bool Get(string key, bool defaultValue) => Storage.Get(key, defaultValue);
+        public Vector2 Get(string key, Vector2 defaultValue) => Storage.Get(key, defaultValue);
+        public Vector3 Get(string key, Vector3 defaultValue) => Storage.Get(key, defaultValue);
+        public Vector4 Get(string key, Vector4 defaultValue) => Storage.Get(key, defaultValue);
+        public Quaternion Get(string key, Quaternion defaultValue) => Storage.Get(key, defaultValue);
+        public Color Get(string key, Color defaultValue) => Storage.Get(key, defaultValue);
+        public Color32 Get(string key, Color32 defaultValue) => Storage.Get(key, defaultValue);
 
-        public float Get(string key, float defaultValue) => _storage.Get(key, defaultValue);
-        public string Get(string key, string defaultValue) => _storage.Get(key, defaultValue);
-        public int Get(string key, int defaultValue) => _storage.Get(key, defaultValue);
-        public bool Get(string key, bool defaultValue) => _storage.Get(key, defaultValue);
-        public Vector2 Get(string key, Vector2 defaultValue) => _storage.Get(key, defaultValue);
-        public Vector3 Get(string key, Vector3 defaultValue) => _storage.Get(key, defaultValue);
-        public Vector4 Get(string key, Vector4 defaultValue) => _storage.Get(key, defaultValue);
-        public Quaternion Get(string key, Quaternion defaultValue) => _storage.Get(key, defaultValue);
-        public Color Get(string key, Color defaultValue) => _storage.Get(key, defaultValue);
-        public Color32 Get(string key, Color32 defaultValue) => _storage.Get(key, defaultValue);
+        public void Clear() => Storage.Clear();
 
-        public void Clear() => _storage.Clear();
+        private bool IsVector2(JSONNode jsonNode)
+        {
+            return jsonNode.Count == 2 && jsonNode["x"] != null && jsonNode["y"] != null;
+        }
+
+        private bool IsVector3(JSONNode jsonNode)
+        {
+            return jsonNode.Count == 3 && jsonNode["x"] != null && jsonNode["y"] != null && jsonNode["z"] != null;
+        }
+
+        private bool IsQuaternion(JSONNode jsonNode)
+        {
+            return jsonNode.Count == 4 && jsonNode["x"] != null && jsonNode["y"] != null && jsonNode["z"] != null && jsonNode["w"] != null;
+        }
+
+        private bool IsColor(JSONNode jsonNode)
+        {
+            return jsonNode.Count == 4 && jsonNode["r"] != null && jsonNode["g"] != null && jsonNode["b"] != null && jsonNode["a"] != null;
+        }
+
+        private bool IsHexColorString(string value)
+        {
+            return value.StartsWith("#") && (value.Length == 7);
+        }
+
+        private bool IsRgbaColorString(string value)
+        {
+            return value.StartsWith("rgba(") && value.EndsWith(")");
+        }
+
+        private Color HexToColor(string hex)
+        {
+            if (ColorUtility.TryParseHtmlString(hex, out var color)) {
+                return color;
+            }
+
+            return Color.white;
+        }
+
+        private Color RgbaToColor(string rgba)
+        {
+            var values = rgba.Replace("rgba(", "").Replace(")", "").Split(',');
+            if (values.Length == 4) {
+                return new Color(
+                    float.Parse(values[0]) / 255f,
+                    float.Parse(values[1]) / 255f,
+                    float.Parse(values[2]) / 255f,
+                    float.Parse(values[3]) / 255f
+                );
+            }
+
+            return Color.white;
+        }
     }
 }
